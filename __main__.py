839e8c89e0ace9e87a1234a69ffd62885b7db71e
Nick <ntabler@ualberta.ca>
2019-04-10 12:42:59 -0600

    Integrate UDP reading and plotting

--- __main__.py
+++ __main__.py
@@ -17,59 +17,92 @@ lat = {}
 lng = {}
 labels = {}
 
-data = pd.read_csv('data.csv')
+partial = ''
+data = pd.DataFrame(columns=['StationID',
+                             'Date',
+                             'Time',
+                             'S1',
+                             'S2',
+                             'S3',
+                             'S4',
+                             'S5',
+                             'S6',
+                             'S7',
+                             'Lattitude',
+                             'Longitude',
+                             'Battery'
+                             ])
+
+connection = mav.connect()
+
+#data = pd.read_csv('data.csv')
 
 fig = plt.figure()
+    
 
 def animate(i):
     plt.clf()
 
-    stationID = data['StationID'][i]
+    global partial
+    global data
 
-    lat[stationID] = data['Lattitude'][i]
-    lng[stationID] = data['Longitude'][i]
+    partial, data = mav.get_line(connection, partial, data)
+    
+    #print(len(data.index))
 
-    j = 0 
-    for meas in measureables:
-        meas[stationID] = data[meas_str[j]][i]
-        j = j + 1 
+    if len(data.index) > 2:
 
-    lngx = list(lng.values())
-    laty = list(lat.values())
+        last = len(data.index) - 1
 
-    xdiff = np.amax(lngx) - np.amin(lngx)
-    ydiff = np.amax(laty) - np.amin(laty)
+        stationID = data['StationID'][last]
 
-    j = 1
-    for vals in measureables:
-        for item in vals.items():
-            labels[item[0]] = str(item[0]) + ": " + str(item[1])
+        lat[stationID] = float(data['Lattitude'][last])
+        lng[stationID] = float(data['Longitude'][last])
 
-        vals_list = list(vals.values())
-        labels_list = list(labels.values())
+        j = 0 
+        for meas in measureables:
+            if data[meas_str[j]][last] is not 'E':
+                meas[stationID] = float(data[meas_str[j]][last])
+            else: 
+                meas[stationID] = float(data[meas_str[j]][last-1])
+            j = j + 1 
 
-        print(vals)
+        lngx = list(lng.values())
+        laty = list(lat.values())
 
-        plt.subplot(3,3,j)
-        plt.scatter(lngx, laty, c=vals_list, s=100, cmap='viridis')
+        xdiff = np.amax(lngx) - np.amin(lngx)
+        ydiff = np.amax(laty) - np.amin(laty)
 
-        plt.xlim(np.amin(lngx) - 0.2*xdiff, np.amax(lngx) + 0.2*xdiff)
-        plt.ylim(np.amin(laty) - 0.2*ydiff, np.amax(laty) + 0.2*ydiff)
+        j = 1
+        for vals in measureables:
+            for item in vals.items():
+                labels[item[0]] = str(item[0]) + ": " + str(item[1])
 
-        plt.xlabel('Longitude')
-        plt.ylabel('Latitude')
+            vals_list = list(vals.values())
+            labels_list = list(labels.values())
 
-        plt.yticks([])
-        plt.xticks([])
+            #print(vals)
 
-        for label, x, y in zip(labels_list, lngx, laty):
-            plt.annotate(
-                label,
-                xy=(x, y), xytext=(-20, 20),
-                textcoords='offset points', ha='right', va='bottom',
-                bbox=dict(boxstyle='round,pad=0.5', fc='yellow', alpha=0.5),
-                arrowprops=dict(arrowstyle = '->', connectionstyle='arc3,rad=0'))
-        j = j+1
+            plt.subplot(3,3,j)
+            plt.scatter(lngx, laty, c=vals_list, s=100, cmap='viridis')
+
+            plt.xlim(np.amin(lngx) - 0.2*xdiff, np.amax(lngx) + 0.2*xdiff)
+            plt.ylim(np.amin(laty) - 0.2*ydiff, np.amax(laty) + 0.2*ydiff)
+
+            plt.xlabel('Longitude')
+            plt.ylabel('Latitude')
+
+            plt.yticks([])
+            plt.xticks([])
+
+            for label, x, y in zip(labels_list, lngx, laty):
+                plt.annotate(
+                    label,
+                    xy=(x, y), xytext=(-20, 20),
+                    textcoords='offset points', ha='right', va='bottom',
+                    bbox=dict(boxstyle='round,pad=0.5', fc='yellow', alpha=0.5),
+                    arrowprops=dict(arrowstyle = '->', connectionstyle='arc3,rad=0'))
+            j = j+1
 
 animate = anim.FuncAnimation(fig, animate)
 plt.show()
--- mavlink_udp.py
+++ mavlink_udp.py
@@ -1,8 +1,9 @@
 from pymavlink import mavutil
+import pandas as pd
 
 def connect():
     # Start a connection listening to a UDP port
-    mav = mavutil.mavlink_connection('udp:127.0.0.1:14550')
+    mav = mavutil.mavlink_connection('udp:127.0.0.1:14560')
 
     # Wait for the first heartbeat 
     #   This sets the system and component ID of remote system for the link
@@ -15,10 +16,62 @@ def connect():
 def get_data64(mav):
     # Once connected, use 'mav' to get and send messages
     data64 = mav.recv_match(type='DATA64', blocking=True)
-    print(data64.data)
+    ascii64 = ''.join(chr(i) for i in data64.data)
+    #print(ascii64)
+    #print(data64.data)
 
-    return data64.data
+    return ascii64
+
+def get_line(connection, partial, arr):
+    line = get_data64(connection)
+    lines = line.splitlines()
+    #print(lines)
+    partial = partial + lines[0]
+    partial = partial.replace('\x00','')
+    data = partial.split(',')
+    print(len(data))
+    if len(data) < 13 and len(lines) > 1:
+        partial = lines[1]
+    elif len(lines) > 1:
+        arr = arr.append(pd.DataFrame([data],
+                             columns=['StationID',
+                             'Date',
+                             'Time',
+                             'S1',
+                             'S2',
+                             'S3',
+                             'S4',
+                             'S5',
+                             'S6',
+                             'S7',
+                             'Lattitude',
+                             'Longitude',
+                             'Battery'
+                             ]),
+                             ignore_index=True
+        )
+        partial = lines[1]
+    print(arr)
+    return partial, arr
 
 if __name__ == "__main__":
     connection = connect()
-    get_data64(connection)
+    #get_data64(connection)
+    partial = ''
+    data = pd.DataFrame(columns=['StationID',
+                             'Date',
+                             'Time',
+                             'S1',
+                             'S2',
+                             'S3',
+                             'S4',
+                             'S5',
+                             'S6',
+                             'S7',
+                             'Lattitude',
+                             'Longitude',
+                             'Battery'
+                             ])
+    while True:
+        #ascii64 = get_data64(connection)
+        partial, data = get_line(connection, partial, data)
